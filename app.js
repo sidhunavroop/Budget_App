b4e7065caccb4e0de64d343e513c8988803abd9c
Navroop Singh Sidhu <sidhunavroop@yahoo.com>
2019-06-23 18:50:35 -0400

    Reading input data and changed app name

--- app.js
+++ app.js
@@ -4,22 +4,43 @@ var budgetController = (function() {
 })();
 
 var UIController = (function() {
-  //Some code
+
+  var DOMstrings = {
+    inputType: '.add__type',
+    inputDescription: '.add__description',
+    inputValue: '.add__value',
+    inputBtn: '.add__btn'
+  };
+  
+  return {
+    getInput: function(){
+      return {
+        type: document.querySelector(DOMstrings.inputType).value,
+        description: document.querySelector(DOMstrings.inputDescription).value,
+        value: document.querySelector(DOMstrings.inputValue).value
+      };      
+    },
+
+    getDOMstrings: function() {
+      return DOMstrings;
+    }
+  };
 })(); 
 
 
 var controller = (function(budgetCtrl, UICtrl) {
-
+  var DOM = UICtrl.getDOMstrings();
   var ctrlAddItem = function(){
     // Input data
+    var input = UICtrl.getInput();
+    console.log(input);
     // add item to budget controller
-    // Add item to the UI
+    // Add item to the UI 
     // calculate the budget
     // display the budget on the UI
-    console.log("hello");
   }
 
-  document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+  document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
 
   document.addEventListener('keypress', function(event){
     if (event.keycode === 13 || event.which === 13 ) {
