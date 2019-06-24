c323543c161336bfbf2b4d6e7826b39d9b6b9e6e
Navroop Singh Sidhu <sidhunavroop@yahoo.com>
2019-06-24 03:15:25 -0400

    Final touch and improving the UX

--- app.js
+++ app.js
@@ -176,6 +176,12 @@ var UIController = (function() {
     return (type === 'expense' ? '-' : '+') + ' ' + int + '.' + dec;
 
   };
+
+  var nodeListForEach = function(list, callback) {
+    for (var i = 0; i < list.length; i++) {
+      callback(list[i], i);
+    }
+  };
   
   return {
     getInput: function(){
@@ -238,12 +244,6 @@ var UIController = (function() {
 
     displayPercentages: function(percentages){
       var fields = document.querySelectorAll(DOMstrings.expensesPercLabel);
-
-      var nodeListForEach = function(list, callback) {
-        for (var i = 0; i < list.length; i++) {
-          callback(list[i], i);
-        }
-      };
       nodeListForEach(fields, function(current, index) {
         if (percentages[index] > 0) {
           current.textContent = percentages[index] + '%';
@@ -261,6 +261,19 @@ var UIController = (function() {
       month = now.getMonth();
       year = now.getFullYear();
       document.querySelector(DOMstrings.dateLabel).textContent = months[month] + ' ' + year;
+    },
+
+    changedType: function() {
+      var fields = document.querySelectorAll(
+        DOMstrings.inputType + ',' +
+        DOMstrings.inputDescription + ',' +
+        DOMstrings.inputValue);
+
+      nodeListForEach(fields, function(current) {
+        current.classList.toggle('red-focus');
+      });
+
+      document.querySelector(DOMstrings.inputBtn).classList.toggle('red');
     },
 
     getDOMstrings: function() {
@@ -284,6 +297,7 @@ var controller = (function(budgetCtrl, UICtrl) {
     });
 
     document.querySelector(DOM.container).addEventListener('click', ctrlDeleteItem);
+    document.querySelector(DOM.inputType).addEventListener('change', UICtrl.changedType);
   };  
 
   var updateBudget = function() {
