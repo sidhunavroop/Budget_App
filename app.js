d0c69bf296b7a68f9b924377ba902528fa52d7d5
Navroop Singh Sidhu <sidhunavroop@yahoo.com>
2019-06-24 01:26:44 -0400

    Delete an item changes in budgetcontroller and UI

--- app.js
+++ app.js
@@ -59,6 +59,21 @@ var budgetController = (function() {
       return newItem;
     }, 
 
+    deleteItem: function(type, id) {
+      var ids, index;
+
+      ids = data.allItems[type].map(function(current) {
+        return current.id;
+      });
+
+      index = ids.indexOf(id);
+
+      if (index !== -1) {
+        data.allItems[type].splice(index, 1);
+      }
+
+    },
+
     calculateBudget: function() {
       //calculate total income and expenses
       calculateTotal('expense');
@@ -135,6 +150,11 @@ var UIController = (function() {
 
       //Insert the HTML into the DOM
       document.querySelector(element).insertAdjacentHTML('beforeend', newHtml);
+    },
+
+    deleteListItem: function(selectorID) {
+      el = document.getElementById(selectorID);
+      el.parentNode.removeChild(el);
     },
 
     clearFields: function() {
@@ -219,11 +239,15 @@ var controller = (function(budgetCtrl, UICtrl) {
     if (itemID) {
       splitID = itemID.split('-');
       type = splitID[0];
-      ID = splitID[1];
+      ID = parseInt(splitID[1]);
 
       // delete the item from the data structure
+      budgetCtrl.deleteItem(type, ID);
       // delete the item from UI
+      UICtrl.deleteListItem(itemID);
+
       // update and show the new budget
+      updateBudget();
     }
   };
   return {
